d8a7ffb9fbe014723ea95981333bab6ee4c457d9
Shripad Rao <shripada0987@gmail.com>
2022-10-28 23:14:58 +0530

    move index.html

---
+++ backend/sheet.py
@@ -0,0 +1,4 @@
+""" integration with google sheets, also includes queries.
+    Uses api/currentDayTime for necessary queries."""
+
+"""1st column, rows are Days. 1st row, columns are time slots in 24 hour format(imp)."""
--- main.py
+++ main.py
@@ -3,24 +3,38 @@ app = Flask(__name__)
 import pyrebase
 import requests
 import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 """example output:  {'display_text': ['hello!'], 'clock': ['on'], 'TimeTable': ['Sat', '16:33:41']} """
 
 
-firebase_config = {"apiKey": "AIzaSyDMAxF4jqNKGy1zyV6GMgT2qxYbr1mG6bY",
+# firebase_config = {"apiKey": "AIzaSyDMAxF4jqNKGy1zyV6GMgT2qxYbr1mG6bY",
 
-  "authDomain": "gpd-2-fab7c.firebaseapp.com",
+#   "authDomain": "gpd-2-fab7c.firebaseapp.com",
 
-  "projectId": "gpd-2-fab7c",
+#   "projectId": "gpd-2-fab7c",
 
-  "storageBucket": "gpd-2-fab7c.appspot.com",
+#   "storageBucket": "gpd-2-fab7c.appspot.com",
 
-  "messagingSenderId": "1061276271847",
+#   "messagingSenderId": "1061276271847",
 
-  "appId": "1:1061276271847:web:69662a45954d944b78ce42",
+#   "appId": "1:1061276271847:web:69662a45954d944b78ce42",
 
-    "databaseURL" : "https://gpd-2-fab7c-default-rtdb.firebaseio.com/"
+#     "databaseURL" : "https://gpd-2-fab7c-default-rtdb.firebaseio.com/"
+# }
+
+firebase_config = {
+   "apiKey" : os.getenv('API_KEY'),
+   "authDomain" : os.getenv("authDomain"),
+   "projectId" : os.getenv("projectId"),
+   "storageBucket" : os.getenv("storageBucket"),
+   "messagingSenderId" : os.getenv("messagingSenderId"),
+   "appId" : os.getenv("appId"),
+   "databaseURL" : os.getenv("databaseURL")
 }
+print(firebase_config)
 
 firebase = pyrebase.initialize_app(firebase_config)
 
@@ -37,19 +51,6 @@ def fetchTimeTableApi():
    resp_data_dict = {"TimeTable":json.loads(response_data_parsed)}
    return(resp_data_dict)
 
-# def parseImmutableDict(form_data):
-#    print("response: ", form_data)
-#    form_data_str= str(form_data)
-#    form_data_str= form_data_str[20:-2]
-#    form_data_str.replace("(","[")
-#    form_data_str.replace(")","]")
-#    print("form_data_str: ", form_data_str)
-#    form_data_parsed = list(form_data_str)
-#   # print("form_data_parsed: ", form_data_parsed)
-#    #print(form_data_parsed)
-   # return(str(form_data))
-
-#print(fetchTimeTableApi())
 
 @app.route('/api',methods = ['POST', 'GET'])
 def result():
@@ -57,10 +58,8 @@ def result():
       form_data = request.form.to_dict(flat=False)
       result = form_data  
       result.update(fetchTimeTableApi()) 
-      print(result)
-      #print(jsonify(result))
-      #db.push(result)
-      #return("Response Submitted!" + "Output: " +str(result))
+      #print(result)
+      db.push(result)
       return render_template("result.html")
 
 
